ed6d2712dfc6136527c28a8dd3670452a8061d8a
Henry Zarza <henryacz7@gmail.com>
2023-10-18 18:27:51 +0200

    Navbar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     fontFamily: {
       sans: ['Satoshi', defaultTheme.fontFamily.sans],
