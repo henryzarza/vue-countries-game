d254bb6872b1fd1fb1cb2923d7f76bf0cf282060
Henry Zarza <henryacz7@gmail.com>
2023-10-19 19:50:22 +0200

    Remove unused components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
-import BaseContent from '../components/BaseContent.vue'
 </script>
 
 <template>
-  <BaseContent title="About View">
-    <ul>
-      <li>Hello 1</li>
-      <li>Hello 2</li>
-      <li>Hello 3</li>
-      <li>Hello 4</li>
-      <li>Hello 5</li>
-    </ul>
-  </BaseContent>
+  <h1>About View</h1>
 </template>
--- src/views/FlippingGameView.vue
+++ src/views/FlippingGameView.vue
@@ -1,20 +1,6 @@
 <script setup lang="ts">
-import BaseContent from '../components/BaseContent.vue'
 </script>
 
 <template>
-  <BaseContent title="Flipping Game View">
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui,
-      eligendi totam voluptates consequuntur, dolores dolor voluptatem
-      maxime perferendis aliquid vitae perspiciatis sit labore a
-      officiis corrupti veritatis, voluptas cum ipsum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Ratione itaque numquam iste. Temporibus suscipit animi reprehenderit
-      minus a voluptatibus totam. Neque expedita voluptatum aut.
-      Pariatur odio et eaque inventore distinctio!
-    </p>
-  </BaseContent>
+  <h1>Flipping Game View</h1>
 </template>
