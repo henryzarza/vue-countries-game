93838a815027df292e5699ac3279ef55197545fb
Henry Zarza <henryacz7@gmail.com>
2023-10-25 19:45:28 +0200

    Add country detail type

--- src/types/Home.ts
+++ src/types/Home.ts
@@ -5,6 +5,28 @@ interface Country {
   capital: string;
 }
 
+interface CountryDetail {
+  emoji: string;
+  name: string;
+  capital: string;
+  awsRegion: string;
+  currencies: string[];
+  phone: string;
+  states: {
+    code: string;
+    name: string;
+  }[] | null;
+  continent: {
+    name: string;
+  }
+  languages: {
+    code: string;
+    name: string;
+    native: string;
+  }[]
+}
+
 export {
   type Country,
+  type CountryDetail,
 }
