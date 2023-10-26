dd5ddebd6a67da646afe0df112d38bf9f81916fe
Henry Zarza <henryacz7@gmail.com>
2023-10-26 15:37:47 +0200

    Flipping Game UI layout

---
+++ src/components/Flipping/Card.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import IconIdea from './IconIdea.vue'
+
+const { emoji, isPicked, isGuessed } = defineProps<{
+  emoji: string,
+  isPicked?: boolean,
+  isGuessed?: boolean,
+}>()
+</script>
+
+<template>
+  <button
+    class="
+      h-40 w-full rounded hover:shadow-md transition hover:scale-105
+      relative border-2 border-slate-300 dark:border-slate-600
+      cursor-pointer duration-500 ease-in-out card bg-violet-200 dark:bg-teal-900
+    "
+    :class="{
+      'picked': isPicked,
+      'guessed pointer-events-none': isGuessed
+    }"
+  >
+    <!-- Card front -->
+    <div
+      class="card-face card-face--front absolute inset-0 pointer-events-none flex flex-col items-center justify-center transition-colors"
+      :class="{
+        'bg-slate-200 dark:bg-slate-800': isPicked,
+        'bg-slate-50 dark:bg-slate-700': isGuessed
+      }"
+    >
+      <h6 class="text-8xl leading-[0.9]">
+        {{emoji}}
+      </h6>
+      <span class="text-sm font-semibold text-zinc-950 dark:text-zinc-50">
+        Country Name
+      </span>
+    </div>
+
+    <!-- Card back -->
+    <div class="card-face absolute inset-0 pointer-events-none flex flex-col items-center justify-center">
+      <IconIdea lineClass="fill-zinc-700 dark:fill-zinc-50" />
+    </div>
+  </button>
+</template>
+
+<style scoped>
+.card {
+  transform-style: preserve-3d;
+}
+
+.card-face {
+  backface-visibility: hidden;
+}
+
+.card-face--front,
+.picked {
+  transform: rotateY(180deg);
+}
+
+.guessed {
+  transform: rotateY(180deg) scale(0.9);
+}
+</style>
--- src/views/FlippingGameView.vue
+++ src/views/FlippingGameView.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
+import Card from '@/components/Flipping/Card.vue'
 </script>
 
 <template>
-  <h1>Flipping Game View</h1>
+  <h1 class="text-4xl font-bold text-zinc-950 dark:text-zinc-50 mb-4 text-center">
+    Flipping Game
+  </h1>
+  <p class="text-base text-zinc-950 dark:text-zinc-50 max-w-xl w-full mx-auto mb-4">
+    The idea of this memory game is: you click on a card, then the card flips over,
+    you will see an emoji with the flag and the name of the country, then you click
+    on a different card and if it matches (they are the same), well you have a <strong class="font-semibold">match</strong>.
+    If there is no match both will turn over, so you must memorize the position of each
+    country on the card to win the game. <strong class="font-semibold">Enjoy it and good luck!</strong>
+  </p>
+  <h6 class="flex items-center justify-center text-base text-zinc-950 dark:text-zinc-50 mb-6">
+    Movements: <span class="text-4xl font-semibold ml-3">25</span>
+  </h6>
+  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-4">
+    <Card emoji="🇲🇰" />
+    <Card emoji="🇲🇽" />
+    <Card emoji="🇩🇪" />
+    <Card emoji="🇦🇱" />
+    <Card emoji="🇸🇮" is-guessed />
+    <Card emoji="🇨🇴" />
+    <Card emoji="🇨🇿" />
+    <Card emoji="🇧🇦" is-picked />
+    <Card emoji="🇷🇸" />
+    <Card emoji="🇭🇷" />
+  </div>
 </template>
