57fffed2df43e8f01e444f91d2d9014e883e6c80
Henry Zarza <henryacz7@gmail.com>
2023-11-07 21:22:36 +0200

    Continent game logic and UI

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -2,8 +2,27 @@ interface Country {
   code: string;
   emoji: string;
   name: string;
+  continent?: {
+    code: CONTINENTS_CODES;
+  }
+}
+
+interface Continent {
+  code: CONTINENTS_CODES;
+  name: string;
+}
+
+export enum CONTINENTS_CODES {
+  AF = 'AF',
+  AN = 'AN',
+  AS = 'AS',
+  EU = 'EU',
+  NA = 'NA',
+  OC = 'OC',
+  SA = 'SA',
 }
 
 export {
   type Country,
+  type Continent,
 }
