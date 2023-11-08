b766bc85f8332b3493130ae1fed4d587c7eae505
Henry Zarza <henryacz7@gmail.com>
2023-11-08 18:23:49 +0200

    Form controls components

---
+++ src/components/Forms/FormRadioCheckbox.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const { nameId, options, type, label } = defineProps<{
+  nameId: string,
+  options: { id: string, name: string }[],
+  type: 'radio' | 'checkbox',
+  label: string,
+}>()
+</script>
+
+<template>
+  <label class="text-sm font-medium mb-1 text-zinc-950 dark:text-zinc-50" :for="nameId">
+    {{ label }}
+  </label>
+  <div class="flex flex-wrap gap-3">
+    <div class="flex gap-1" v-for="option in options" :key="option.id">
+      <input class="custom-input hidden" :type="type" :name="nameId" :id="`${nameId}-${option.id}`" :value="option.id">
+      <label
+        :for="`${nameId}-${option.id}`"
+        class="
+          text-md text-zinc-950 dark:text-zinc-50 p-2 ring-1 ring-inset ring-zinc-300
+          bg-zinc-100 dark:ring-zinc-500 dark:bg-zinc-700 rounded-sm
+          cursor-pointer transition-all hover:bg-violet-200 hover:shadow-md hover:ring-violet-300
+          dark:hover:bg-emerald-900 dark:hover:ring-emerald-700 hover:font-semibold
+        "
+      >
+        {{option.name}}
+      </label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.custom-input:checked + label {
+  box-shadow: inset 2px 2px 2px rgb(0 0 0 / 0.25), inset -2px -2px 2px rgb(0 0 0 / 0.2);
+  font-weight: 600;
+  transform: scale(0.9);
+}
+</style>
---
+++ src/components/Forms/FormSelect.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const { nameId, label, placeholder, options } = defineProps<{
+  nameId: string,
+  label: string,
+  placeholder?: string,
+  options: { id: string, name: string }[]
+}>()
+</script>
+
+<template>
+  <label
+    :for="nameId"
+    class="text-sm font-medium mb-1 text-zinc-950 dark:text-zinc-50 capitalize"
+  >
+    {{label}}
+  </label>
+  <select
+    :name="nameId"
+    :id="nameId"
+    class="
+      w-full rounded-sm border-0 py-2 px-3 text-zinc-950 dark:text-zinc-50 shadow-sm
+      ring-1 ring-inset ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset
+      focus:ring-violet-600 dark:focus:ring-emerald-600 bg-zinc-50 dark:bg-zinc-800
+      dark:placeholder:text-zinc-500 dark:ring-zinc-700 outline-none
+    "
+  >
+    <option v-if="placeholder" value="">{{ placeholder }}</option>
+    <option v-for="option in options" :key="option.id" :value="option.id">
+      {{ option.name }}
+    </option>
+  </select>
+</template>
