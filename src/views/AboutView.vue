83acd2df7a9a1036da9a48ae150d2400667c7cf9
Henry Zarza <henryacz7@gmail.com>
2023-11-09 18:40:17 +0200

    About UI: form validations integration

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import gql from 'graphql-tag'
 import { useQuery } from '@vue/apollo-composable'
-import { computed } from 'vue'
+import { computed, reactive } from 'vue'
+import { useVuelidate } from '@vuelidate/core'
 
 import FormInput from '@/components/Forms/FormInput.vue'
 import FormRadioCheckbox from '@/components/Forms/FormRadioCheckbox.vue'
 import FormSelect from '@/components/Forms/FormSelect.vue'
 import FormTextarea from '@/components/Forms/FormTextarea.vue'
+import FormError from '@/components/Forms/FormError.vue'
 import type { Country } from '@/types/Home'
+import { aboutFormFields, aboutFormRules } from '@/constants'
 
+const CUISINE_COUNTRIES = [
+  { id: 'AR', name: '🇦🇷 Argentinian' },
+  { id: 'CO', name: '🇨🇴 Colombian' },
+  { id: 'MX', name: '🇲🇽 Mexican' },
+  { id: 'PE', name: '🇵🇪 Peruvian' },
+]
 const SA_COUNTRIES_QUERY = gql`
   query aboutSACountries {
     continents(filter: { code: { eq: "SA" } }) {
@@ -20,7 +29,6 @@ const SA_COUNTRIES_QUERY = gql`
     }
   }
 `
-
 const { result } = useQuery<{ continents: { countries: Omit<Country, 'capital'>[] }[] }>(SA_COUNTRIES_QUERY)
 
 const filterCountries = computed(
@@ -32,15 +40,19 @@ const filterCountries = computed(
     return []
   })
 
-const CUISINE_COUNTRIES = [
-  { id: 'AR', name: '🇦🇷 Argentinian' },
-  { id: 'CO', name: '🇨🇴 Colombian' },
-  { id: 'MX', name: '🇲🇽 Mexican' },
-  { id: 'PE', name: '🇵🇪 Peruvian' },
-]
+const formState = reactive(aboutFormFields)
+const v$ = useVuelidate(aboutFormRules, formState)
+
+const updateCheckboxValue = (value: string) => {
+  if (formState.andeanCountries.has(value)) {
+    formState.andeanCountries.delete(value)
+  } else {
+    formState.andeanCountries.add(value)
+  }
+}
 
 const onSubmit = () => {
-  console.log('validate')
+  v$.value.$touch()
 }
 
 </script>
@@ -53,77 +65,149 @@ const onSubmit = () => {
     This page is just for testing and learning purposes about working with forms validations
     in VueJS, it's me just playing around 🤷
   </p>
-  <!-- Interactions buttons -->
-  <form class="flex flex-col w-full max-w-md gap-5 mx-auto" @submit.prevent="onSubmit">
-    <div class="flex flex-col">
-      <FormInput label="Full Name" nameId="fullName" placeholder="Jane Doe" />
-    </div>
-    <div class="flex flex-col">
-      <FormInput label="Email" nameId="email" placeholder="jane_doe@example.io" autocomplete="email" />
-    </div>
-    <div class="flex flex-col sm:flex-row gap-4">
+  <div class="flex flex-col md:flex-row gap-4 justify-center items-start">
+    <form class="flex flex-col w-full max-w-md gap-5 mx-auto" @submit.prevent="onSubmit">
       <div class="flex flex-col">
-        <FormInput label="How many countries have you visited?" nameId="countriesVisited" placeholder="7" type="number" />
+        <FormInput
+          label="Full Name"
+          nameId="fullName"
+          placeholder="Jane Doe"
+          v-model="formState.fullName"
+          :hasError="v$.fullName.$errors.length > 0"
+          isRequired
+        >
+          <FormError :errors="v$.fullName.$errors" />
+        </FormInput>
       </div>
       <div class="flex flex-col">
-        <FormInput label="How many countries did you visit this year?" nameId="countriesThisYear" placeholder="2" type="number" />
+        <FormInput
+          label="Email"
+          nameId="email"
+          placeholder="jane_doe@example.io"
+          autocomplete="email"
+          v-model="formState.email"
+          :hasError="v$.email.$errors.length > 0"
+          isRequired
+        >
+          <FormError :errors="v$.email.$errors" />
+        </FormInput>
       </div>
+      <div class="flex flex-col sm:flex-row gap-4">
+        <div class="flex flex-col flex-1">
+          <FormInput
+            label="How many countries have you visited?"
+            nameId="countriesVisited"
+            placeholder="7"
+            type="number"
+            v-model="formState.countriesVisited"
+            :hasError="v$.countriesVisited.$errors.length > 0"
+            isRequired
+          >
+            <FormError :errors="v$.countriesVisited.$errors" />
+          </FormInput>
+        </div>
+        <div class="flex flex-col flex-1">
+          <FormInput
+            label="How many countries did you visit this year?"
+            nameId="countriesThisYear"
+            placeholder="2"
+            type="number"
+            v-model="formState.countriesThisYear"
+            :hasError="v$.countriesThisYear.$errors.length > 0"
+            isRequired
+          >
+            <FormError :errors="v$.countriesThisYear.$errors" />
+          </FormInput>
+        </div>
+      </div>
+      <div class="flex flex-col">
+        <FormSelect
+          label="Favorite South America Country"
+          nameId="favoriteCountry"
+          placeholder="Which country..."
+          :options="filterCountries"
+          :hasError="v$.favoriteCountry.$errors.length > 0"
+          v-model="formState.favoriteCountry"
+          isRequired
+        >
+          <FormError :errors="v$.favoriteCountry.$errors" />
+        </FormSelect>
+      </div>
+      <div class="flex flex-col">
+        <FormSelect
+          label="Least favorite South America Country"
+          nameId="leastFavoriteCountry"
+          placeholder="Which country..."
+          :options="filterCountries"
+          v-model="formState.leastFavoriteCountry"
+        />
+      </div>
+      <div class="flex flex-col">
+        <FormRadioCheckbox
+          label="What is your favorite cuisine?"
+          nameId="favoriteFood"
+          type="radio"
+          :options="CUISINE_COUNTRIES"
+          @updateValue="value => formState.favoriteFood = value"
+          isRequired
+        >
+          <FormError :errors="v$.favoriteFood.$errors" />
+        </FormRadioCheckbox>
+      </div>
+      <div class="flex flex-col">
+        <FormRadioCheckbox
+          label="Which countries are Andean?"
+          nameId="andeanCountries"
+          type="checkbox"
+          :options="filterCountries"
+          @updateValue="updateCheckboxValue"
+        />
+      </div>
+      <div class="flex flex-col">
+        <FormInput
+          label="Password"
+          nameId="password"
+          placeholder="••••••••••"
+          type="password"
+          v-model="formState.password"
+          :hasError="v$.password.$errors.length > 0"
+          isRequired
+        >
+          <FormError :errors="v$.password.$errors" />
+        </FormInput>
+      </div>
+      <div class="flex flex-col">
+        <FormTextarea
+          label="A random content"
+          nameId="randomContent"
+          placeholder="Lorem ipsum..."
+          :hasError="v$.randomContent.$errors.length > 0"
+          v-model="formState.randomContent"
+        >
+          <FormError :errors="v$.randomContent.$errors" />
+        </FormTextarea>
+      </div>
+  
+      <button
+        type="submit"
+        class="
+          text-base text-zinc-50 py-2 px-3 rounded-sm transition-colors mt-6
+          bg-violet-800 font-bold hover:text-zinc-50 dark:bg-emerald-700
+          hover:bg-violet-600 dark:hover:bg-emerald-600
+        "
+      >
+        Let's Go
+      </button>
+    </form>
+  
+    <div class="flex flex-col w-full max-w-md sticky top-24" v-if="!v$.$invalid">
+      <h6 class="text-lg font-bold text-zinc-950 dark:text-zinc-50 mb-2">Form is Valid 🥳</h6>
+      <pre
+        class="
+          w-full border-2 border-zinc-300 bg-zinc-100 p-3 dark:bg-zinc-800
+          dark:border-zinc-400 border-dashed text-zinc-950 dark:text-zinc-50
+        "
+      >{{ JSON.stringify({ ...formState, andeanCountries: Array.from(formState.andeanCountries) }, undefined, 2) }}</pre>
     </div>
-    <div class="flex flex-col">
-      <FormSelect
-        label="Favorite South America Country"
-        nameId="favoriteCountry"
-        placeholder="Which country..."
-        :options="filterCountries"
-      />
-    </div>
-    <div class="flex flex-col">
-      <FormSelect
-        label="Least favorite South America Country"
-        nameId="leastFavoriteCountry"
-        placeholder="Which country..."
-        :options="filterCountries"
-      />
-    </div>
-    <div class="flex flex-col">
-      <FormRadioCheckbox
-        label="What is your favorite cuisine?"
-        nameId="favoriteFood"
-        type="radio"
-        :options="CUISINE_COUNTRIES"
-      />
-    </div>
-    <div class="flex flex-col">
-      <FormRadioCheckbox
-        label="Which countries are Andean?"
-        nameId="andeanCountries"
-        type="checkbox"
-        :options="filterCountries"
-      />
-    </div>
-    <div class="flex flex-col">
-      <FormInput label="Password" nameId="password" placeholder="••••••••••" type="password" />
-    </div>
-    <div class="flex flex-col">
-      <FormInput label="Repeat Password" nameId="repeatPassword" placeholder="••••••••••" type="password" />
-    </div>
-    <div class="flex flex-col">
-      <FormTextarea
-        label="A random content"
-        nameId="randomContent"
-        placeholder="Lorem ipsum..."
-      />
-    </div>
-
-    <button
-      type="submit"
-      class="
-        text-base text-zinc-50 py-2 px-3 rounded-sm transition-colors mt-6
-        bg-violet-800 font-bold hover:text-zinc-50 dark:bg-emerald-700
-        hover:bg-violet-600 dark:hover:bg-emerald-600
-      "
-    >
-      Let's Go
-    </button>
-  </form>
+  </div>
 </template>
